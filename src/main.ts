b8c0729968ff059d34b00cdf63d8d091457f1ac5
ghost160111 <jalol100.ghost@yandex.com>
2024-10-13 19:39:33 +0500

    added some changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import "./style.css";
 
+const accessToken: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhMjk4Yjc3ZS1jYTJkLTRkYWMtYmZlNy1kYzA4YjgyMTdiMjAiLCJpZCI6MjQ3MDY2LCJpYXQiOjE3Mjg1Mzk3NDZ9.-7JmcxLSYtut_Z4ZQKln0gB306vlLJZeiVMQSRbuJUk";
+
 // @ts-expect-error test
-Cesium.Ion.defaultAccessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhMjk4Yjc3ZS1jYTJkLTRkYWMtYmZlNy1kYzA4YjgyMTdiMjAiLCJpZCI6MjQ3MDY2LCJpYXQiOjE3Mjg1Mzk3NDZ9.-7JmcxLSYtut_Z4ZQKln0gB306vlLJZeiVMQSRbuJUk";
+Cesium.Ion.defaultAccessToken = accessToken;
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 // @ts-expect-error test
